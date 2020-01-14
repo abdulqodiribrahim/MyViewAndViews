89dcd56ac2f8f3cccd191057d9ea4d863a71fa60
abdulqodiribrahim <abdulqodiribrahim@yahoo.com>
2020-01-15 00:45:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myviewandviews/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnjoss;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setTitle("creating of Abdul qodir ibrahim");
+        }
+        btnjoss=findViewById(R.id.donat);
+        btnjoss.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.donat:
+                Intent kintil =new Intent(this,DonasiActivity.class);
+                startActivity(kintil);
+                break;
+        }
+    }
+}
